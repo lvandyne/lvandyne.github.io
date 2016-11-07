0d320abb862a638662db76b95f71559c8b6db1bc
Lindsay Shay <lshay@moresteam.com>
2016-11-07 14:54:49 -0500

    add html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 $('h1').html('Hello from js!');
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+    zoomControl: false;
+    fullscreenControl: true;
+  });
+}
