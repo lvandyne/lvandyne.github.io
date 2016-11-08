1b7c7f601b285246d6795ecc0460f1ffdfd80fb3
Lindsay Shay <lshay@moresteam.com>
2016-11-08 10:32:10 -0500

    add map and marker

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function initMap() {
+  var position = {lat: 40.8054491, lng: -73.9654415};
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.8054491, lng: -73.9654415},
-    zoom: 8
+    center: position,
+    zoom: 12
+  });
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map
   });
 }
-initMap();
